560bf7bcab0f8bba41b62c0e6a7de67502488aa9
compuYael <aguilar.eduardo32@yahoo.com>
2020-04-22 12:48:40 -0500

    a

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ app.use(cors({
 
 
 const AuthRoutes = require('./routes/AuthRoutes');
-app.use('/auth', AuthRoutes);
+app.use('api/auth', AuthRoutes);
 const TripRoutes = require('./routes/TripRoutes');
 app.use('/trip', TripRoutes);
 
--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -2,7 +2,7 @@ const express  = require('express');
 const router = express.Router();
 const AuthController = require('../controllers/AuthController')
 
-router.post('/signup', AuthController.signup);
+router.post('/api/signup', AuthController.signup);
 router.post('/login', AuthController.login);
 router.post('/logout', AuthController.logout);
 router.get('/loggedin', AuthController.loggedin);
